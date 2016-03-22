96262fca740b18943e85594c9093590a21709675
BPclaofo <alan.clark3@bp.com>
2016-03-22 13:01:32 +0000

    Update Base.java

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -7,7 +7,7 @@ class Base {
 
     protected void run() {
         double r = Math.random();
-        if (r < 0.01) {
+        if (r < 0.5) {
             fail("oops");
         } else if (r < 0.2) {
             throw new AssumptionViolatedException("skipping");
